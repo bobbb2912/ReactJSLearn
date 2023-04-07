d446d9896d743d7bec1139b53d0defe9bef856ce
bobbb2912 <nthien2912@gmail.com>
2023-04-07 13:07:47 +0700

    redux filter

--- Buoi hoc/react-app/src/redux_test/components/Post.js
+++ Buoi hoc/react-app/src/redux_test/components/Post.js
@@ -1,23 +1,63 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Button, Card, Row } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux'
-import { deletePost, filterPost } from '../feature/postSlice';
+import { deletePost, filterPost, resetPost } from '../feature/postSlice';
 
 export default function Post() {
     const posts = useSelector((state) => state.postReducer);
     const dispatch = useDispatch();
+    const [postLocal, setPostLocal] = useState(null);
     const deleteItem = (id) => {
         dispatch(deletePost(id));
     };
-    const filter = () => {
-        dispatch(filterPost(3));
+    const filter = (rate) => {
+        let convertPost = JSON.parse(JSON.stringify(postLocal.data));
+        convertPost = convertPost.filter((i) => i.rate > rate)
+        setPostLocal(convertPost);
     };
+    const filterRedux = (rate) => {
+        dispatch(filterPost(rate));
+
+    };
+    const reset = () => {
+        setPostLocal(posts);
+    }
+    const resetFilterRedux = () => {
+        dispatch(resetPost());
+    }
+
+    useEffect(() => {
+        setPostLocal(posts);
+    
+    }, [posts]);
   return (
     <div>
         <h1>POST LIST</h1>
-        <Button onClick={filter}>Filter post</Button>
+        <Button onClick={() => filter(3)}>Filter post state</Button>
+        <Button style={{marginLeft:'10px', marginRight:'10px'}} 
+         onClick={() => reset()}>Reset post</Button>
+        <Button style={{marginLeft:'10px', marginRight:'10px'}} 
+        onClick={() => filterRedux(3)}>Filter post redux</Button>
+        <Button style={{marginLeft:'10px', marginRight:'10px'}} 
+        onClick={() => resetFilterRedux()}>Reset filter post redux</Button>
+
         <Row>
-        {posts.map((item, index) => {
+        {posts.dataFilter ? posts.dataFilter.map((item, index) => {
+            return (
+                 
+                <Card key={item.id} style={{ width: '18rem', marginRight:'10px', marginBottom:'10px' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>{item.content}</Card.Text>
+                    <Button onClick={() => deleteItem(item.id)} variant="danger">Delete</Button>
+                </Card.Body>
+                </Card>
+                
+            
+            );
+        }): 
+        posts.data.map((item, index) => {
             return (
                  
                 <Card key={item.id} style={{ width: '18rem', marginRight:'10px', marginBottom:'10px' }}>
--- Buoi hoc/react-app/src/redux_test/feature/postSlice.js
+++ Buoi hoc/react-app/src/redux_test/feature/postSlice.js
@@ -1,12 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-    {id:'1', content:'Day la bai viet so 1', title:'Bai viet 1', rate: 3},
-    {id:'2', content:'Hello', title:'Bai viet 2', rate: 2},
-    {id:'3', content:'Irure eiusmod qui qui est commodo eu dolor nostrud pariatur exercitation fugiat Lorem qui sint.', title:'Bai viet 3', rate: 5},
-    {id:'4', content:'Amet consequat ut sunt nulla nulla excepteur sint nostrud consequat est excepteur pariatur duis eiusmod. Sit dolore excepteur minim est quis sunt labore dolore aliquip nulla consequat ex tempor. Consequat et pariatur Lorem ipsum do labore amet irure cupidatat. Nostrud ut aliqua ut fugiat elit eu nulla pariatur enim dolore sunt velit aliquip. Ex Lorem nostrud magna et laboris qui mollit cillum magna nulla reprehenderit.', title:'Bai viet 4', rate: 5},
+const initialState = {
+    data:[
+        {id:'1', content:'Day la bai viet so 1', title:'Bai viet 1', rate: 3},
+        {id:'2', content:'Hello', title:'Bai viet 2', rate: 2},
+        {id:'3', content:'Irure eiusmod qui qui est commodo eu dolor nostrud pariatur exercitation fugiat Lorem qui sint.', title:'Bai viet 3', rate: 5},
+        {id:'4', content:'Amet consequat ut sunt nulla nulla excepteur sint nostrud consequat est excepteur pariatur duis eiusmod. Sit dolore excepteur minim est quis sunt labore dolore aliquip nulla consequat ex tempor. Consequat et pariatur Lorem ipsum do labore amet irure cupidatat. Nostrud ut aliqua ut fugiat elit eu nulla pariatur enim dolore sunt velit aliquip. Ex Lorem nostrud magna et laboris qui mollit cillum magna nulla reprehenderit.', title:'Bai viet 4', rate: 5},
 
-]
+    ],
+    dataFilter:null
+}
 
 const postSlice = createSlice({
   name: "post",
@@ -33,12 +36,17 @@ const postSlice = createSlice({
     filterPost: (state, action) => {
 // loc tat ca ca bai post co rate >= action.payload
         const convertData = JSON.parse(JSON.stringify(state));
-        const dataFilter = convertData.filter((i) => i.rate>=action.payload);
-        return dataFilter;
-    }
+        const dataFilter = convertData.data.filter((i) => i.rate>action.payload);
+         convertData.dataFilter = dataFilter;
+        return convertData;
+    },
+    resetPost: (state, action) => {
+// loc tat ca ca bai post co rate >= action.payload
+        state.dataFilter = null;
+    },
   }
 });
 
-export const {addPost, deletePost, editPost, filterPost} = postSlice.actions
+export const {addPost, deletePost, editPost, filterPost, resetPost} = postSlice.actions
 
 export default postSlice.reducer
